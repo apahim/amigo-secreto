a3008c4290f3940423cc0aa7adf2e5afebe38b48
Amador Pahim <apahim@redhat.com>
2022-10-13 21:00:49 +0100

    db
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+class Friends(models.Model):
+    token = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    available = models.BooleanField()
+
+
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
--- hello/views.py
+++ hello/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .models import Friends
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     para = request.GET.get('message')
     return render(request, "index.html", {'para': para})
+
+
+def sorteio(request):
+    error = None
+    token = request.GET.get('token')
+    user = Friends.objects.filter(token=token)
+    if user is None:
+        error = "Usuário não encontrado. Entre em contato com o admin."
+
+    return render(request, "sorteio.html", {'token': token, 'error': error})
 
 
 def db(request):
